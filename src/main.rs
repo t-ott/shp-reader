4248bdff1798d739f1fde1df34f897e4c7ed17a3
Tommy Ott <tommy.ott617@gmail.com>
2023-06-15 19:10:23 -0400

    Read shp file code

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 // Esri Shapefile Specification:
 // https://www.esri.com/content/dam/esrisites/sitecore-archive/Files/Pdfs/library/whitepapers/pdfs/shapefile.pdf
 
-use std::io::{self, Read};
+use std::io::{self, Cursor, Read};
 use std::fs::File;
+use byteorder::{BigEndian, ByteOrder, LittleEndian, ReadBytesExt};
 
 static TEST_FILE: &str = "/home/tott/layers/numeric_poly_test_3857.shp";
 
 fn main() -> io::Result<()> {
     let mut f = File::open(TEST_FILE)?;
-    let mut buffer = [0; 92];
-    println!("buffer: {:?}", buffer);
+    let mut buf = [0; 1];
+    println!("buffer before: {:?}", buf);
 
-    f.read(&mut buffer)?;
+    f.read_i32_into::<BigEndian>(&mut buf)?;
 
-    println!("buffer: {:?}", buffer);
+    println!("buffer after: {:?}", buf);
     Ok(())
 }

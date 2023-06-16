c74256035e2889066065a406a6e6432604819123
Tommy Ott <tommy.ott617@gmail.com>
2023-06-15 20:02:03 -0400

    Switch to BufReader

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,59 @@
 // Esri Shapefile Specification:
 // https://www.esri.com/content/dam/esrisites/sitecore-archive/Files/Pdfs/library/whitepapers/pdfs/shapefile.pdf
 
-use std::io::{self, Cursor, Read};
+use std::io::{self, BufReader, Read};
 use std::fs::File;
-use byteorder::{BigEndian, ByteOrder, LittleEndian, ReadBytesExt};
+// use byteorder::{BigEndian, ByteOrder, LittleEndian, ReadBytesExt};
 
-static TEST_FILE: &str = "/home/tott/layers/numeric_poly_test_3857.shp";
+const TEST_FILE: &str = "/home/tott/layers/numeric_poly_test_3857.shp";
+
+enum ShapeType {
+    NullShape,
+    Point,
+    PolyLine,
+    Polygon,
+    MultiPoint,
+    PointZ,
+    PolyLineZ,
+    PolygonZ,
+    MultiPointZ,
+    PointM,
+    PolyLineM,
+    PolygonM,
+    MultiPointM,
+    MultiPatch
+}
 
 fn main() -> io::Result<()> {
-    let mut f = File::open(TEST_FILE)?;
-    let mut buf = [0; 1];
-    println!("buffer before: {:?}", buf);
+    let f = File::open(TEST_FILE)?;
+    let mut input = BufReader::new(f);
 
-    f.read_i32_into::<BigEndian>(&mut buf)?;
+    let mut i: u8 = 0;
 
-    println!("buffer after: {:?}", buf);
+    // Big Endian header portion
+    while i <= 6 {
+        let mut buf = [0; 4];
+        input.read_exact(&mut buf)?;
+    
+        let n = i32::from_be_bytes(buf);
+        println!("{:?}", n);
+
+        i += 1;
+    }
+
+    // Little Endian header portion
+    loop {
+        let mut buf = [0; 4];
+        input.read_exact(&mut buf)?;
+
+        let n = i32::from_le_bytes(buf);
+        println!{"{:?}", n};
+
+        i += 1;
+        if i == 16 {
+            break;
+        }
+    }
+
     Ok(())
 }
